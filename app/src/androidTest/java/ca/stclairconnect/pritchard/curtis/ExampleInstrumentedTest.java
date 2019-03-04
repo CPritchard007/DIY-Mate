7ad985e6777c8bb11ce3663ecbcf2ab446f04dde
curtis pritchard <cpritchard649@gmail.com>
2019-03-04 18:17:40 -0500

    rewritting the initial android build, launch error resolved.

--- app/src/androidTest/java/ca/stclairconnect/pritchard/curtis/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/ca/stclairconnect/pritchard/curtis/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package com.example.diymate;
+package ca.stclairconnect.pritchard.curtis;
 
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.example.diymate", appContext.getPackageName());
+        assertEquals("ca.stclairconnect.pritchard.curtis", appContext.getPackageName());
     }
 }
